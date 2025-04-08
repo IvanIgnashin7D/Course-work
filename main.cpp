2b7af4bfbf683c9f32f3f7f29f1e348e77b8dc61
Ivanignashin <ivanignashin@mail.ru>
2025-04-08 19:16:03 +0300

    main func + some little fixes

--- main.cpp
+++ main.cpp
@@ -3,14 +3,27 @@
 #include <algorithm>
 
 
-int knapsackBruteForce(int W, std::vector<int>& weights, std::vector<int>& values, int n); // grubaya sila
-int knapsackDP(int W, std::vector<int>& weights, std::vector<int>& values, int n); // dinamicheskoe programmirovanie
-int knapsackBacktracking(int W, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value); // poisk s vozvratom
-int knapsackBranchAndBound(int W, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value, int& best_value); // vetvi i granitsi
+int knapsackBruteForce(int w, std::vector<int>& weights, std::vector<int>& values, int n); // grubaya sila
+int knapsackDP(int w, std::vector<int>& weights, std::vector<int>& values, int n); // dinamicheskoe programmirovanie
+int knapsackBacktracking(int w, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value); // poisk s vozvratom
+int knapsackBranchAndBound(int w, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value, int& best_value); // vetvi i granitsi
 
 
 int main() {
-	std::cout << "Hello world" << '\n';
+    std::setlocale(0, "");
+
+    std::vector<int> values = { 60, 100, 120 };
+    std::vector<int> weights = { 10, 20, 30 };
+    int w = 50;
+    int n = values.size();
+
+    std::cout << "Метод грубой силы:                    " << knapsackBruteForce(w, weights, values, n) << '\n';
+    std::cout << "Метод динамического программирования: " << knapsackDP(w, weights, values, n) << '\n';
+    std::cout << "Метод поиска с возвратом:             " << knapsackBacktracking(w, weights, values, 0, 0, 0) << '\n';
+
+    int best_value = 0;
+    std::cout << "Метод ветвей и границ:                " << knapsackBranchAndBound(w, weights, values, 0, 0, 0, best_value) << '\n';
+
     return 0;
 }
 
@@ -33,10 +46,10 @@ int knapsackBruteForce(int W, std::vector<int>& weights, std::vector<int>& value
 }
 
 
-int knapsackDP(int W, std::vector<int>& weights, std::vector<int>& values, int n) {
-    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(W + 1, 0));
+int knapsackDP(int w, std::vector<int>& weights, std::vector<int>& values, int n) {
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(w + 1, 0));
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= W; j++) {
+        for (int j = 1; j <= w; j++) {
             if (weights[i - 1] <= j) {
                 dp[i][j] = std::max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]]);
             }
@@ -45,33 +58,34 @@ int knapsackDP(int W, std::vector<int>& weights, std::vector<int>& values, int n
             }
         }
     }
-    return dp[n][W];
+    return dp[n][w];
 }
 
-
-int knapsackBacktracking(int W, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value) {
-    if (i == weights.size() || current_weight >= W) {
+// values  = { 60, 100, 120 };
+// weights = { 10, 20,  30  };
+int knapsackBacktracking(int w, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value) {
+    if (i == weights.size() || current_weight >= w) {
         return current_value;
     }
-    if (current_weight + weights[i] > W) {
-        return knapsackBacktracking(W, weights, values, i + 1, current_weight, current_value);
+    if (current_weight + weights[i] > w) {
+        return knapsackBacktracking(w, weights, values, i + 1, current_weight, current_value);
     }
     return std::max(
-        knapsackBacktracking(W, weights, values, i + 1, current_weight + weights[i], current_value + values[i]),
-        knapsackBacktracking(W, weights, values, i + 1, current_weight, current_value)
+        knapsackBacktracking(w, weights, values, i + 1, current_weight + weights[i], current_value + values[i]),
+        knapsackBacktracking(w, weights, values, i + 1, current_weight, current_value)
     );
 }
 
 
-int knapsackBranchAndBound(int W, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value, int& best_value) {
-    if (current_weight <= W && current_value > best_value) {
+int knapsackBranchAndBound(int w, std::vector<int>& weights, std::vector<int>& values, int i, int current_weight, int current_value, int& best_value) {
+    if (current_weight <= w && current_value > best_value) {
         best_value = current_value;
     }
-    if (i == weights.size() || current_weight >= W) {
+    if (i == weights.size() || current_weight >= w) {
         return best_value;
     }
     double bound = current_value;
-    int remaining_weight = W - current_weight;
+    int remaining_weight = w - current_weight;
     int j = i;
     while (j < weights.size() && weights[j] <= remaining_weight) {
         remaining_weight -= weights[j];
@@ -84,7 +98,7 @@ int knapsackBranchAndBound(int W, std::vector<int>& weights, std::vector<int>& v
     if (bound <= best_value) {
         return best_value;
     }
-    knapsackBranchAndBound(W, weights, values, i + 1, current_weight + weights[i], current_value + values[i], best_value);
-    knapsackBranchAndBound(W, weights, values, i + 1, current_weight, current_value, best_value);
+    knapsackBranchAndBound(w, weights, values, i + 1, current_weight + weights[i], current_value + values[i], best_value);
+    knapsackBranchAndBound(w, weights, values, i + 1, current_weight, current_value, best_value);
     return best_value;
 }
